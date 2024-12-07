baf333b92c9b08955bde697b802d6b8fa6f932ec
ArtdragonXoX <1262922587@qq.com>
2024-12-07 21:42:52 +0800

    完善抽象类。

--- Assets/Scripts/Entity/Trap/AbstractTrap.cs
+++ Assets/Scripts/Entity/Trap/AbstractTrap.cs
@@ -4,4 +4,15 @@ using UnityEngine;
 
 public abstract class AbstractTrap : AbstractEntity
 {
+    public new EntityType entityType = EntityType.Trap;
+
+    protected override float Damage()
+    {
+        return attack;
+    }
+
+    protected override void Die()
+    {
+        throw new System.NotImplementedException();
+    }
 }
